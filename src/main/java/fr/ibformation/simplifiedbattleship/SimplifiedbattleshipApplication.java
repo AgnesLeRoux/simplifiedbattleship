e3117900b917cd488ff11d14458c088b061b4073
AgnesLeRoux <anges.leroux@gmail.com>
2017-06-25 15:16:46 +0200

    initialize project

---
+++ src/main/java/fr/ibformation/simplifiedbattleship/SimplifiedbattleshipApplication.java
@@ -0,0 +1,12 @@
+package fr.ibformation.simplifiedbattleship;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimplifiedbattleshipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimplifiedbattleshipApplication.class, args);
+	}
+}
---
+++ src/test/java/fr/ibformation/simplifiedbattleship/SimplifiedbattleshipApplicationTests.java
@@ -0,0 +1,16 @@
+package fr.ibformation.simplifiedbattleship;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SimplifiedbattleshipApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
